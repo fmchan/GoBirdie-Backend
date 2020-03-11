659ccdf1a9243c91b35b523f066cb929b101e4ad
fmchan <formingchan@gmail.com>
2020-03-11 11:19:03 +0800

    change css to use asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
-    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" href="{{asset('css/custom.css') }}">
 
     @yield('css')
 </head>
